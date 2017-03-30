8e90477b2b88ea1277a8c0ac75c5910237e71614
wzding <wzding@users.noreply.github.com>
2017-03-29 19:53:59 -0700

    inorder

---
+++ InOrderTraverse.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def getMinimumDifference(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.min = float('inf')
+        self.previous = float('inf')
+            
+        def inorder(root):
+            if root:
+                inorder(root.left)
+                self.min = min(self.min, abs(root.val - self.previous))
+                self.previous = root.val
+                inorder(root.right)
+        
+        inorder(root)
+        return self.min
