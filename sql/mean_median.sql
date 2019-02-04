62fa6f134d5597fc982ef2653498a877a7f7d7db
emma <emma@Zihengs-MacBook-Pro.local>
2019-02-03 19:11:35 -0800

    added a different method using window function

--- sql/mean_median.sql
+++ sql/mean_median.sql
@@ -43,6 +43,80 @@ FROM (
 ) tmp;
 
 /*
+求 median salary at each company
+https://leetcode.com/problems/median-employee-salary/
+*/
+CREATE TABLE Employee (
+    Id INT NOT NULL,
+    Company VARCHAR(1) NOT NULL,
+    Salary INT NOT NULL
+);
+
+INSERT INTO Employee(Id, Company, Salary)
+VALUES (1, 'A',2341),
+(2, 'A',341),
+(3, 'A',15),
+(4, 'A',15314),
+(5, 'A',451),
+(6, 'A',513),
+(7, 'B',15),
+(8, 'B',13),
+(9, 'B',1154),
+(10,'B',1345),
+(11,'B',1221),
+(12,'B',234),
+(13,'C',2345),
+(14,'C',2645),
+(15,'C',2645),
+(16,'C',2652),
+(17,'C',65);
+-- use window function 非常简单
+-- 注意题目要求只显示处于中间位置的值 不需要计算最终的median
+select Id, Company, Salary
+from (
+  select *,
+  row_number() over(partition by Company order by Salary) as row_a,
+  row_number() over(partition by Company order by Salary desc) as row_b
+  from Employee
+)tmp
+where row_a between row_b - 1 and row_b + 1
+order by Company, Salary
+
+-- 计算每个公司的 median
+select Company, avg(Salary)
+from (
+  select *,
+  row_number() over(partition by Company order by Salary) as row_a,
+  row_number() over(partition by Company order by Salary desc) as row_b
+  from Employee
+)tmp
+where row_a between row_b - 1 and row_b + 1
+group by Company
+order by Company
+
+-- Not use window function 复杂
+-- 注意题目要求只显示处于中间位置的值 不需要计算最终的median
+select Id, tmp.Company, Salary
+from (
+    select *,
+    @curr := if(@prev = Company, @curr := @curr + 1, 1) as rank_num,
+    @prev := Company
+    from
+    Employee,
+    (select @curr := 1, @prev := Null) init
+    order by Company, Salary
+) tmp
+left join
+(select Company,
+ count(*) as total
+ from Employee
+ group by Company
+) cnt
+on tmp.Company = cnt.Company
+where total % 2 = 0 and rank_num between total / 2 and total / 2 + 1
+or total % 2 = 1 and rank_num = (total + 1) / 2
+
+/*
 知道frequency 并不是单独的数字
 不用 window functions
 必须 consider previous cumulative frequency
@@ -71,27 +145,3 @@ from (
 ) tmp
 where curr >= floor((total+1)/2)
 and prev <= total - floor((total+1)/2)
-
-/*
-延伸一下 求 median salary at each company
-https://leetcode.com/problems/median-employee-salary/
-*/
-select Id, tmp.Company, Salary
-from (
-    select *,
-    @curr := if(@prev = Company, @curr := @curr + 1, 1) as rank_num,
-    @prev := Company
-    from
-    Employee,
-    (select @curr := 1, @prev := Null) init
-    order by Company, Salary
-) tmp
-left join
-(select Company,
- count(*) as total
- from Employee
- group by Company
-) cnt
-on tmp.Company = cnt.Company
-where total % 2 = 0 and rank_num between total / 2 and total / 2 + 1
-or total % 2 = 1 and rank_num = (total + 1) / 2
