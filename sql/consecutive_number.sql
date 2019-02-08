14c37fec8ee2e6ae824b09f631fe846ee5daff08
wzding <wzding@berkeley.edu>
2019-02-07 22:41:09 -0800

    no need to join

--- sql/consecutive_number.sql
+++ sql/consecutive_number.sql
@@ -46,12 +46,12 @@ where cnt >= 2;
 
 -- consecutive available seats https://leetcode.com/problems/consecutive-available-seats/
 select distinct c1.seat_id
-from cinema c1
-join cinema c2
-on c1.free = 1 and c2.free = 1
+from cinema c1,
+cinema c2
+where c1.free = 1 and c2.free = 1
 # two consecutive seat
 and abs(c1.seat_id - c2.seat_id) = 1
-order by seat_id
+order by seat_id;
 
 /*
 write a query to display the records which have 3 or more consecutive rows
