4dccad318c7609b0218caec13eaa766276da66ba
wzding <wzding@berkeley.edu>
2019-02-25 11:22:38 -0800

    removed useless join

--- sql/Challenges.sql
+++ sql/Challenges.sql
@@ -4,30 +4,30 @@ print the hacker_id, name, and the total number of challenges created by each st
 
 If more than one student created the same number of challenges and the count is less than the maximum number of challenges created, then exclude those students from the result.
 */
-SELECT H.hacker_id, H.name, COUNT(*) AS total
-FROM Hackers H
-INNER JOIN Challenges C
-ON H.hacker_id = C.hacker_id
-GROUP BY H.hacker_id, H.name
-HAVING total = (
-    SELECT COUNT(*) as cnt1
-    FROM Challenges
-    GROUP BY hacker_id
-    ORDER BY COUNT(*) DESC
-    LIMIT 1
+select h.hacker_id, h.name, count(c.challenge_id) as total
+from Challenges c
+join Hackers h
+on c.hacker_id = h.hacker_id
+group by h.hacker_id, h.name
+having total = (
+    select count(*)
+    from Challenges
+    group by hacker_id
+    order by count(*) desc
+    limit 1
 )
 or total in (
-    SELECT DISTINCT cnt2 FROM (
-        SELECT COUNT(*) AS cnt2
-        FROM Hackers
-        INNER JOIN Challenges
-        ON Hackers.hacker_id = Challenges.hacker_id
-        GROUP BY Hackers.hacker_id, Hackers.name
-    ) cnt_table
-    GROUP BY cnt2
-    HAVING COUNT(cnt2) = 1
-)
-ORDER BY total DESC, H.hacker_id
+    select cnt
+    from (
+      select count(challenge_id) as cnt
+      from Challenges
+      group by hacker_id
+    ) tmp
+    group by cnt
+    having count(cnt) = 1
+  )
+order by total desc, h.hacker_id
+
 
 --- change how to choose max
 SELECT H.hacker_id, H.name, COUNT(*) AS total
@@ -44,14 +44,13 @@ HAVING total = (
     ) temp
 )
 or total in (
-    SELECT DISTINCT cnt2 FROM (
-        SELECT COUNT(*) AS cnt2
-        FROM Hackers
-        INNER JOIN Challenges
-        ON Hackers.hacker_id = Challenges.hacker_id
-        GROUP BY Hackers.hacker_id, Hackers.name
-    ) cnt_table
-    GROUP BY cnt2
-    HAVING COUNT(cnt2) = 1
-)
-ORDER BY total DESC, H.hacker_id
+    select cnt
+    from (
+      select count(challenge_id) as cnt
+      from Challenges
+      group by hacker_id
+    ) tmp
+    group by cnt
+    having count(cnt) = 1
+  )
+order by total desc, h.hacker_id
