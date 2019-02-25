ed13e00083e08c0c75a4c76d96cedc467995bc45
wzding <wzding@berkeley.edu>
2019-02-25 12:53:34 -0800

    added comment

--- sql/cum_sum.sql
+++ sql/cum_sum.sql
@@ -47,12 +47,13 @@ VALUES (1,'2014-10-31', 15),
 /*
 Write a query that returns the total amount of money spent by each user.
 That is, the sum of the column transaction_amount for each user over both tables.
+注意一定要 union all
 */
 SELECT user_id, SUM(transaction_amount) as s
 from (
-(SELECT * FROM march)
-UNION ALL
-(SELECT * FROM april)
+  (SELECT * FROM march)
+  UNION ALL
+  (SELECT * FROM april)
 ) all_data
 GROUP BY user_id
 -- +---------+-------------------------+
@@ -64,31 +65,22 @@ GROUP BY user_id
 -- |      12 |                      13 |
 -- |      13 |                      30 |
 -- +---------+-------------------------+
+
 /*
 returns day by day the cumulative sum of money spent by each user. That is,
 each day a user had a transcation, we should have how much money she has
 spent in total until that day.
+注意要使用 distinct 否则结果有重复
 */
-
-SELECT user_id,
-  p_date,
-  SUM(amount) over (
-    partition by user_id order by p_date
-  ) as cum_sum
-FROM
-(
-  (SELECT user_id, p_date,
-   sum(transaction_amount) as amount
-   FROM march
-    group by user_id, p_date)
-  UNION ALL
-  (SELECT user_id, p_date,
-   sum(transaction_amount) as amount
-   FROM april
-   group by user_id, p_date)
-) all_data
+select distinct user_id,
+p_date,
+sum(transaction_amount) over(partition by user_id order by p_date) as cum_sum
+from (
+  (select * from march)
+  union all
+  (select * from april)
+) march_april
 order by user_id, p_date;
-
 -- +---------+------------+---------+
 -- | user_id | p_date     | cum_sum |
 -- +---------+------------+---------+
@@ -112,18 +104,18 @@ order by user_id, p_date;
 Find Cumulative Salary of an Employee
 https://leetcode.com/problems/find-cumulative-salary-of-an-employee/
 */
-select e1.Id,
+select
+e1.Id,
 e1.Month,
 ifnull(e1.Salary, 0) + ifnull(e2.Salary, 0) + ifnull(e3.Salary, 0) as Salary
-from
-    Employee e1
-    left join Employee e2
-    -- last month
-    on e1.Id = e2.Id and e1.Month = e2.Month + 1
-    left join Employee e3
-    -- month before last month
-    on e1.Id = e3.Id and e1.Month = e3.Month + 2
+from Employee e1
+left join Employee e2
+-- last month
+on e1.Id = e2.Id and e1.Month = e2.Month + 1
+left join Employee e3
+-- month before last month
+on e1.Id = e3.Id and e1.Month = e3.Month + 2
 -- remove most recent month
 where (e1.Id, e1.Month) not in
 (select Id, max(Month) from Employee group by Id)
-order by Id, Month desc
+order by Id, Month desc;
