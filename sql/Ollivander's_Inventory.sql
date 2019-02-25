6bf1d547fb8aea249ed421468f034832b95a0450
wzding <wzding@berkeley.edu>
2019-02-25 11:25:39 -0800

    renamed table

--- sql/Ollivander's_Inventory.sql
+++ sql/Ollivander's_Inventory.sql
@@ -4,15 +4,16 @@ Write a query to print the id, age, coins_needed, and power of the wands that
 Ron's interested in, sorted in order of descending power.
 If more than one wand has same power, sort the result in order of descending age.
 */
-SELECT W.id, P.age, W.coins_needed, W.power
-FROM Wands W
-INNER JOIN Wands_Property P
-ON W.code = P.code
-WHERE P.is_evil = 0 AND
-W.coins_needed = (
-    SELECT MIN(Wands.coins_needed) FROM
-    Wands INNER JOIN Wands_Property
-    ON Wands.code = Wands_Property.code
-    WHERE Wands.power = W.power and Wands_Property.age = P.age
+select id, age, coins_needed, power
+from Wands w
+join Wands_Property p
+on w.code = p.code
+where p.is_evil = 0
+and coins_needed = (
+  select min(coins_needed)
+  from Wands w1
+  join Wands_Property p1
+  on w1.code = p1.code
+  where w1.power = w.power and p1.age = p.age
 )
-ORDER BY W.power DESC, P.age DESC
+order by power desc, age desc
